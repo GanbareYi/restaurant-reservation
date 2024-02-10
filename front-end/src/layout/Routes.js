3173e723a0a39ab6170a4aaeb14820036c015b56
Yi <wuyikm@gmail.com>
2024-02-10 11:19:28 -0800

    Adding code

--- front-end/src/layout/Routes.js
+++ front-end/src/layout/Routes.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import { Redirect, Route, Switch } from "react-router-dom";
 import Dashboard from "../dashboard/Dashboard";
-import Reservation from "../reservations/Reservation";
+import Reservation from "../reservation/Reservation";
 import NotFound from "./NotFound";
 import { today } from "../utils/date-time";
 
@@ -19,13 +19,12 @@ function Routes() {
       <Route exact={true} path="/">
         <Redirect to={"/dashboard"} />
       </Route>
-      <Route exact={true} path="/reservations/new">
-        <Reservation />
-      </Route>
       <Route path="/dashboard">
         <Dashboard date={today()} />
       </Route>
-      
+      <Route exact={true} path="/reservations/new">
+        <Reservation />
+      </Route>
       <Route>
         <NotFound />
       </Route>
