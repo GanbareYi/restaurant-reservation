cb2b9fb2477a47529296092df1f7285a0ae5304f
Yi <wuyikm@gmail.com>
2024-02-16 12:50:23 -0800

    Rename folder.

--- front-end/src/layout/Routes.js
+++ front-end/src/layout/Routes.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import { Redirect, Route, Switch } from "react-router-dom";
 import Dashboard from "../dashboard/Dashboard";
-import Reservation from "../reservation/Reservation";
+import Reservation from "../reservations/Reservation";
 import NotFound from "./NotFound";
 import { today } from "../utils/date-time";
 import useQuery from "../utils/useQuery";
